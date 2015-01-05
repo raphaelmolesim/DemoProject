b314c7624cf2e61f3f624592f9f57e7bb6e4a116
Raphael Molesim <raphael.molesim@lambda3.com.br>
2015-01-05 18:10:41 -0200

    Jokenpo project

---
+++ DemoProject.Core/Judge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.Core
+{
+    public class Judge
+    {
+        public Play Winner(Play play1, Play play2)
+        {
+            var rules = new List<List<Play>>()
+            {
+                new List<Play>() { Play.Rock, Play.Scissor }
+            };
+            var set = new SortedSet<Play>(new Play[] { play1, play2 });
+
+            if (set.First() == Play.Scissor && set.Last() == Play.Rock)
+                return set.Last();
+            if (set.First() == Play.Papper && set.Last() == Play.Rock)
+                return set.First();
+            if (set.First() == Play.Scissor && set.Last() == Play.Papper)
+                return set.First();
+
+            throw new InvalidOperationException("Unkonw element causing invalid state");
+        }
+    }
+
+    public enum Play
+    {
+        Scissor = 1, Papper = 2, Rock = 3
+    }
+}
---
+++ DemoProject.Tests/JudgeTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoProject.Core;
+
+namespace DemoProject.Tests
+{
+    [TestClass]
+    public class JudgeTest
+    {
+        [TestMethod]
+        public void ScissorsWins()
+        {
+            var expected = Play.Scissor;
+            var actual = new Judge().Winner(Play.Scissor, Play.Papper);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ScissorsWinsInAnyOrderOfInputs()
+        {
+            var expected = Play.Scissor;
+            var actual = new Judge().Winner(Play.Papper, Play.Scissor);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PapperWins()
+        {
+            var expected = Play.Papper;
+            var actual = new Judge().Winner(Play.Papper, Play.Rock);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PapperWinInAnyOrderOfInputs()
+        {
+            var expected = Play.Papper;
+            var actual = new Judge().Winner(Play.Rock, Play.Papper);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RockWins()
+        {
+            var expected = Play.Rock;
+            var actual = new Judge().Winner(Play.Rock, Play.Scissor);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RockWinsInAnyOrderOfInputs()
+        {
+            var expected = Play.Rock;
+            var actual = new Judge().Winner(Play.Scissor, Play.Rock);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ThrownAnExceptionInCaseOfUnkonwRule()
+        {
+            var actual = new Judge().Winner(Play.Scissor, (Play)5);
+        }
+    }
+}

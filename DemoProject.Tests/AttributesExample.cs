e399a637d3c3d3db6ccaf3bfcf24a9b76b82bc04
Raphael Molesim <raphael.molesim@lambda3.com.br>
2015-01-06 16:48:58 -0200

    Exemplo de Atributos

---
+++ DemoProject.Tests/AttributesExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace DemoProject.Tests
+{
+    [TestClass]
+    public class AttributesExample
+    {
+        private static StringBuilder sb = new StringBuilder();
+
+        [AssemblyInitialize]
+        public static void ExecutaAntesDeRodarTodosOsTestes(TestContext textContext)
+        {
+            sb.AppendLine("-> ExecutaAntesDeRodarTodosOsTestes");
+        }
+
+        [ClassInitialize]
+        public static void ExecutaAntesDeRodarOsTestesDeUmaClasse(TestContext textContext)
+        {
+            sb.AppendLine("--> ExecutaAntesDeRodarOsTestesDeUmaClasse");
+        }
+
+        [TestInitialize]
+        public void ExecutaAntesDeCadaTesteDaClasse()
+        {
+            sb.AppendLine("---> ExecutaAntesDeCadaTesteDaClasse");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            sb.AppendLine("----> TestMethod1");
+            Assert.AreEqual(1, 1);
+            Console.WriteLine(sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            sb.AppendLine("----> TestMethod2");
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestCleanup]
+        public void ExecutaCleanupDepoisDeCadaTesteDaClasse()
+        {
+            sb.AppendLine("--!> ExecutaCleanupDepoisDeCadaTesteDaClasse");
+        }
+
+        [ClassCleanup]
+        public static void ExecutaCleanupDepoisDeTodosOsTestesDaClasse()
+        {
+            sb.AppendLine("-!> ExecutaCleanupDepoisDeTodosOsTestesDaClasse");
+        }
+
+        [AssemblyCleanup]
+        public static void ExecutaCleanupDepoisDeTodosOsTestes()
+        {
+            sb.AppendLine("!> ExecutaCleanupDepoisDeTodosOsTestes");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
